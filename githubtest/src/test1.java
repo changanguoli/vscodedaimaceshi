bd5591d5ad6f3f932f53ff1298ab3fd26b0b02e8
changanguoli <2398953834@qq.com>
2026-04-14 09:41:34 +0800

    second save

--- githubtest/src/test1.java
+++ githubtest/src/test1.java
@@ -1,5 +1,7 @@
 public class test1 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+     System.out.println("Hello, World!");
+
     }
 }
