99e8a881929b891e578e004a3f1eb5b5f2e7ffb2
changanguoli <2398953834@qq.com>
2026-04-14 23:49:28 +0800

    four save

---
+++ githubtest/src/firstpackage/test3.java
@@ -0,0 +1,9 @@
+package firstpackage;
+
+public class test3 {
+  
+  public static void main(String[] args) throws Exception {
+    System.out.println("Hello, World!");
+    System.out.println("This is a test.");
+  }
+}
